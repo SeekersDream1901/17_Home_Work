0682253da83bd209c84ddb88d8d96687b4ff165d
SeekersDream1901 <nikolaigarshin2112@gmail.com>
2022-11-05 11:54:56 +0300

    С первого раза не попало в гитхаб

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+api = Api(app)
+
+movies_ns = api.namespace("movies")
+
 
 class Movie(db.Model):
     __tablename__ = 'movie'
@@ -24,6 +28,7 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +41,49 @@ class Genre(db.Model):
     name = db.Column(db.String(255))
 
 
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+@movies_ns.route("/")
+class MoviesView(Resource):
+    def get(self):
+        movies = Movie.query.all()
+        return movies_schema.dump(movies), 200
+
+    # def post(self):
+    #     req_json = request.json
+    #     movies = Movie.query.all()
+    #     search_movies = []
+    #
+    #     for movie in movies:
+    #         if movie.director_id == req_json["director_id"]:
+    #             search_movies.append(movie)
+    #             print(search_movies)
+    #     # return movies_schema.dump(search_movies)
+
+
+@movies_ns.route("/<int:id>")
+class MoviesView(Resource):
+    def get(self, id):
+        movie = Movie.query.get(id)
+        return movie_schema.dump(movie), 200
+
+
+
+# Доработайте представление так, чтобы оно возвращало только
+# фильмы с определенным режиссером по запросу типа /movies/?director_id=1.
 
 if __name__ == '__main__':
     app.run(debug=True)
